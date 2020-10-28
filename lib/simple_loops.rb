5b8c8775865706c5130dbd771c880a97a351071b
Michael Andrikanich <mandrikanich@gmail.com>
2020-10-28 15:24:30 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,32 @@
-# Write your methods here
+def loop_message_five_times (parameter)
+  count = 0 
+  while count < 5 do 
+  puts parameter 
+  count += 1 
+ end 
+end
+
+def loop_message_n_times (parameter1, parameter2)
+  count = 0 
+  while count < 10 do
+    puts parameter1
+    count += 1 
+  end 
+end 
+
+def output_array (array)
+  count = 0
+  while count < array.length do 
+    puts array 
+    count += 1 
+  end
+end
+
+def return_string_array (array)
+  count = 0 
+  while count < array.length do 
+    count += 1 
+  end 
+end 
+  
+  
